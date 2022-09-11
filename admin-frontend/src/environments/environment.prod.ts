6428202dad6478915e563bd49992c317263e4a66
naixatwork <naixatwork@gmail.com>
2022-09-12 00:09:10 +0430

    refactor(global): made admin connect to new backend

--- admin-frontend/src/environments/environment.prod.ts
+++ admin-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://185.226.117.86:3000/',
+  baseUrl: 'http:/localhost:5000',
 };
--- customer-frontend/src/environments/environment.ts
+++ customer-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:7152/',
+  baseUrl: 'https:/localhost:5001/',
 };
